5959b6eb274f6862add77588429e16bafea3a41b
Mark Baker <shakybaker@gmail.com>
2013-11-05 10:26:45 +0000

    tests pass

---
+++ TennisGameKata2.Model/Game.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisGameKata2.Model
+{
+    public class Game
+    {
+        private int score1 { get; set; }
+        private int score2 { get; set; }
+
+        private bool IsDeuce
+        {
+            get { return ((score1 == 3) && (score2 == 3)); }
+        }
+
+        private bool Player1Wins
+        {
+            get { return (((score1 >= 4) && (score2 < 3)) || ((score1 == 5) && (score2 == 3))); }
+        }
+
+        private bool Player2Wins
+        {
+            get { return (((score2 >= 4) && (score1 < 3)) || ((score2 == 5) && (score1 == 3))); }
+        }
+
+        public Game()
+        {
+            score1 = 0;
+            score2 = 0;
+        }
+
+        public string GetScore()
+        {
+            if (Player1Wins)
+                return "Player 1 Wins";
+            if (Player2Wins)
+                return "Player 2 Wins";
+            if (IsDeuce)
+                return "Deuce";
+
+            return string.Format("{0}, {1}", FormatScore(score1), FormatScore(score2));
+        }
+
+        private string FormatScore(int score)
+        {
+            if (score == 0)
+                return "Love";
+            else if (score == 1)
+                return "15";
+            else if (score == 2)
+                return "30";
+            else if (score == 3)
+                return "40";
+            else if (score == 4)
+                return "Advantage";
+
+            return string.Empty;
+        }
+
+        public void AddScoreToPlayer(int id)
+        {
+            if (id == 1)
+                score1++;
+            else
+                score2++;
+
+            if ((score1 == 4) && (score2 == 4))
+            {
+                score1--;
+                score2--;
+            }
+        }
+    }
+}
---
+++ TennisGameKata2.Test/GameTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TennisGameKata2.Model;
+
+namespace TennisGameKata2.Test
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void Player_score_is_incremented_when_a_point_is_scored()
+        {
+            var game = new Game();
+            game.AddScoreToPlayer(1);
+
+            Assert.AreEqual("15, Love", game.GetScore());
+        }
+
+        [TestMethod]
+        public void Player_scores_4_times_and_opponent_does_not_score_then_player_wins()
+        {
+            var game = new Game();
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+
+            Assert.AreEqual("Player 1 Wins", game.GetScore());
+        }
+
+        [TestMethod]
+        public void Both_players_are_on_40_then_deuce_is_shown_as_score()
+        {
+            var game = new Game();
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(2);
+            game.AddScoreToPlayer(2);
+            game.AddScoreToPlayer(2);
+
+            Assert.AreEqual("Deuce", game.GetScore());
+        }
+
+        [TestMethod]
+        public void Players_on_deuce_and_player_scores_then_advantage_is_shown()
+        {
+            var game = new Game();
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(2);
+            game.AddScoreToPlayer(2);
+            game.AddScoreToPlayer(2);
+            game.AddScoreToPlayer(1);
+
+            Assert.AreEqual("Advantage, 40", game.GetScore());
+        }
+
+        [TestMethod]
+        public void Player_has_advantage_then_opponent_scores_then_deuce_is_shown()
+        {
+            var game = new Game();
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(2);
+            game.AddScoreToPlayer(2);
+            game.AddScoreToPlayer(2);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(2);
+
+            Assert.AreEqual("Deuce", game.GetScore());
+        }
+
+        [TestMethod]
+        public void Player_has_advantage_then_scores_then_player_wins()
+        {
+            var game = new Game();
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(2);
+            game.AddScoreToPlayer(2);
+            game.AddScoreToPlayer(2);
+            game.AddScoreToPlayer(1);
+            game.AddScoreToPlayer(1);
+
+            Assert.AreEqual("Player 1 Wins", game.GetScore());
+        }
+    }
+}
